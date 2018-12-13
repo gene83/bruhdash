2fe260a5b5f2ae146f370c1e2bf5a808d92f3713
Gene83 <gene83@protonmail.com>
2018-12-12 14:08:04 -1000

    first crack
    will refactor later to make functions either mutate or access like lo-dash counterparts

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,84 +10,128 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+    return arr.shift();
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (arr) {
+    return arr.pop();
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr, x) {
+    return arr.indexOf(x);
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (arr, x) {
+    return arr.lastIndexOf(x);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    arr.pop();
+    return arr;
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
+  compact: function(arr) {
+   return arr.filter(value => !!value);
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, index1, index2) {
+    return arr.slice(index1,index2);
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(arr, n){
+    if (n === undefined) {
+      arr.shift();
+    } else {
+      arr.splice(0, n);
+      // return arr.slice(n) also works
+    }
+    return arr;
   },
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
-
+  dropRight: function(arr, n) {
+    if (n === undefined) {
+      arr.pop();
+    } else {
+      arr.splice(arr.length - n, arr.length);
+    }
+    return arr;
   },
 
   // creates a slice of an array with n elements taken from the beginning
-  take: function () {
-
+  take: function (arr, n) {
+    if (n === undefined) {
+      return arr.slice(0, 1);
+    } else {
+      return arr.slice(0, n)
+    }
   },
 
   // creates a slice of an array with n elements taken from the end
-  takeRight: function () {
-
+  takeRight: function (arr, n) {
+    if (n === undefined) {
+      return arr.slice (arr.length - 1, arr.length);
+    } else {
+      return arr.slice(arr.length - n, arr.length);
+    }
   },
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function() {
+  fill: function(arr, val, start, end) {
 
+    if (start === undefined && end === undefined) {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = val;
+      };
+    } else {
+      for (let i = start; i < end; i++) {
+        arr[i] = val;
+      }
+    }
+    return arr;
   },
 
   // removes all given values from an array
-  pull: function () {
-
+  pull: function (arr, value1, value2) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value1 || arr[i] === value2) {
+        arr.splice(i,1);
+        i--
+      }
+    }
+    return arr;
   },
 
   // removes elements of an array corresponding to the given indices
-  pullAt: function () {
-
+  pullAt: function (arr, indexes) {
+    /*  for (let i = 0; i < arr.length; i++) {
+        if (indexes.includes(i)) {
+          arr.splice(i, 1)
+        }
+      }
+      return arr;
+      above doesn't work because its based on indexes and they change with every splice, 
+      I want to use splice to mutate the original array though
+      */
   },
 
   // creates an array excluding all the specified values
-  without: function() {
-
+  without: function(arr, value1, value2) {
+    return arr.filter(value => ![value1, value2].includes(value));
   },
 
   // returns an array with specified values excluded
-  difference: function() {
-
+  difference: function(arr, values) {
+    return arr.filter(val => !values.includes(val));
   },
 
   /*******************
